30f6c43662f003c29ad1615bd910921ca4249672
Matthew Wierenga <matt.wierenga@outlook.com>
2023-09-26 14:59:30 +0200

    Added the Different Scenes

---
+++ app/src/main/java/com/example/myapplication/Timer.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Timer : AppCompatActivity() {
+
+
+    private val timer = Timer()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timer)
+
+
+
+
+    }
+
+
+
+
+
+}
