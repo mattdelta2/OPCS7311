66d9c21d85a46fc12b2fffa1d0dfae37b61c136e
Matthew Wierenga <matt.wierenga@outlook.com>
2023-09-26 15:29:08 +0200

    adding a timer

--- app/src/main/java/com/example/myapplication/MainMenu.kt
+++ app/src/main/java/com/example/myapplication/MainMenu.kt
@@ -1,11 +1,25 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+
+
+        val timerButton = findViewById<Button>(R.id.btnTimer)
+
+        timerButton.setOnClickListener()
+        {
+            val intent = Intent(this, Timer::class.java)
+        }
+
+
+
     }
 }
--- app/src/main/java/com/example/myapplication/Timer.kt
+++ app/src/main/java/com/example/myapplication/Timer.kt
@@ -2,14 +2,63 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class Timer : AppCompatActivity() {
 
 
-    private val timer = Timer()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
+
+
+
+        val timerTextView = findViewById<TextView>(R.id.timerTextView)
+        val startButton = findViewById<Button>(R.id.startButton)
+        val stopButton = findViewById<Button>(R.id.stopButton)
+        val restartButton = findViewById<Button>(R.id.restartButton)
+        var isTimerRunning = false
+        var elapsedMillis = 0L
+        var timer: CountUpTimer? = null
+
+
+        // Set click listeners for your buttons
+        startButton.setOnClickListener {
+            if (!isTimerRunning) {
+                isTimerRunning = true
+                timer = CountUpTimer(0L, 1000) { elapsedMillis ->
+                    val formattedTime = formatElapsedTime(elapsedMillis)
+                    timerTextView.text = formattedTime
+                }
+                timer?.start()
+            }
+        }
+
+        stopButton.setOnClickListener {
+            if (isTimerRunning) {
+                isTimerRunning = false
+                timer?.cancel()
+            }
+        }
+
+        restartButton.setOnClickListener {
+            isTimerRunning = false
+            timer?.cancel()
+            elapsedMillis = 0L
+            timerTextView.text = "00:00:00"
+        }
+    }
+
+    private fun formatElapsedTime(elapsedMillis: Long): String {
+        // Format elapsed time into hours, minutes, and seconds
+        val hours = elapsedMillis / 3600000
+        val minutes = (elapsedMillis % 3600000) / 60000
+        val seconds = (elapsedMillis % 60000) / 1000
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds)
+    }
 
 
 
@@ -20,4 +69,3 @@ class Timer : AppCompatActivity() {
 
 
 
-}
