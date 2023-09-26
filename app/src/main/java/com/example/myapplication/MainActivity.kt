6f100d8e362b8f48e6353316bc8b376418eee1ee
Matthew Wierenga <matt.wierenga@outlook.com>
2023-09-26 15:02:38 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val loginButton = findViewById<Button>(R.id.btm_Login)
+
+        loginButton.setOnClickListener()
+        {
+            handleLogin()
+        }
+    }
+
+
+    private fun handleLogin()
+    {
+
+        if(isLoginSuccessful())
+        {
+            val intent = Intent(this, MainMenu::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    private fun isLoginSuccessful(): Boolean
+    {
+        return true
     }
 }
