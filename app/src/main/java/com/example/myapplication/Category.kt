d49a9f2753d25e62b845f1f086c3d13fdf1cc413
Matthew Wierenga <matt.wierenga@outlook.com>
2023-09-27 10:11:10 +0200

    Added code to save the category and desciption from the user
    added backButton on Category page

--- app/src/main/java/com/example/myapplication/Category.kt
+++ app/src/main/java/com/example/myapplication/Category.kt
@@ -1,11 +1,72 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.myapplication.R.*
 
 class Category : AppCompatActivity() {
+
+
+    private lateinit var categoryEditText: EditText
+    private lateinit var descriptionEditText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_category)
+        setContentView(layout.activity_category)
+
+
+        categoryEditText = findViewById(R.id.categoryEditText)
+        descriptionEditText = findViewById(R.id.descriptionEditText)
+
+
+        val submitButton = findViewById<Button>(R.id.btnSubmit)
+
+
+        val sharedPreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
+
+        submitButton.setOnClickListener()
+            {
+
+                val category = categoryEditText.text.toString()
+                val description = descriptionEditText.text.toString()
+
+
+                val editor = sharedPreferences.edit()
+
+
+
+                editor.putString("Category", category)
+                editor.putString("Description", description)
+
+
+
+                editor.apply()
+
+
+
+                categoryEditText.text.clear()
+                descriptionEditText.text.clear()
+
+            }
+
+
+        var backButton = findViewById<Button>(R.id.btnBack)
+
+
+        backButton.setOnClickListener()
+        {
+            val intent = Intent(this, MainMenu::class.java)
+
+            startActivity(intent)
+        }
+
+
+
+
+        val savedCategory = sharedPreferences.getString("Category", " ")
+        val savedDescription = sharedPreferences.getString("Description", " ")
     }
 }
