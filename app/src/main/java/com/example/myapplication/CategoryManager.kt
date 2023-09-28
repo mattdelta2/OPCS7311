9067bcab654d8dfd66b1a5d8f2fca023929497e3
Python <nadiadupreez3@gmail.com>
2023-09-28 16:03:25 +0200

    Added functions for the image handling

--- app/src/main/java/com/example/myapplication/CategoryManager.kt
+++ app/src/main/java/com/example/myapplication/CategoryManager.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 
@@ -9,6 +11,12 @@ class CategoryManager(private val context: Context) {
     private val sharedPreferences = context.getSharedPreferences("Categories", Context.MODE_PRIVATE)
     private val gson = Gson()
     private var categoryCounter = 1 // Counter for generating unique IDs
+
+    data class CategoryName(
+        val id: Int,
+        val name: String,
+        val imagePath: String? // Add this field to store the image path
+    )
 
     // Function to generate a unique ID
     fun generateUniqueId(): Int {
@@ -35,5 +43,21 @@ class CategoryManager(private val context: Context) {
     private fun saveCategories(categories: List<CategoryName>) {
         val json = gson.toJson(categories)
         sharedPreferences.edit().putString("categories", json).apply()
+    }
+
+    // Function to select an image for a category
+    fun selectImageForCategory(activity: Activity, requestCode: Int) {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        activity.startActivityForResult(intent, requestCode)
+    }
+
+    // Function to handle the result of image selection and update a category
+    fun handleImageSelectionResult(data: Intent?, category: CategoryName?): CategoryName? {
+        if (data != null && data.data != null && category != null) {
+            val selectedImageUri = data.data
+            return category.copy(imagePath = selectedImageUri.toString())
+        }
+        return null
     }
 }
