31c919fa1b0cf5f0e17834a3180145d392b50c94
Matthew Wierenga <matt.wierenga@outlook.com>
2023-09-29 13:49:06 +0200

    Updated UI

--- app/src/main/java/com/example/myapplication/PlayerRoom.kt
+++ app/src/main/java/com/example/myapplication/PlayerRoom.kt
@@ -9,18 +9,46 @@ import android.widget.ImageButton
 class PlayerRoom : AppCompatActivity() {
     private lateinit var backButton: ImageButton
 
+    private lateinit var shopButton: ImageButton
+
+    private lateinit var timerButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_room)
 
         backButton = findViewById(R.id.backBtn)
 
+        shopButton = findViewById((R.id.Shopbtn))
+
+        timerButton = findViewById(R.id.Timerbtn)
+
         backButton.setOnClickListener{(back())}
+
+        shopButton.setOnClickListener{(shop())}
+
+        timerButton.setOnClickListener{(timer())}
+
+
     }
 
     private fun back()
     {
         val intent = Intent(this, MainMenu::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun shop()
+    {
+        val intent = Intent(this, Shop::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun timer()
+    {
+        val intent = Intent(this, Shop::class.java)
         startActivity(intent)
         finish()
     }
